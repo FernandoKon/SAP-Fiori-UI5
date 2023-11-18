0ace3d9b82389406599ae9818f04b378b1b20415
fernando <fehkon@hotmail.com>
2023-11-17 23:49:12 -0300

    aplicando conceitos 1

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,14 +1,52 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.firstapp.controller.Home", {
             onInit: function () {
+
+                const list = [
+                    {
+                        name: "Apple",
+                        color: "Red",
+                        quantity: 5
+                    },
+                    {
+                        name: "Orange",
+                        color: "Orange",
+                        quantity: 9
+                    },
+                    {
+                        name: "Banana",
+                        color: "Yellow",
+                        quantity: 3
+                    },
+                    {
+                        name: "Watermelon",
+                        color: "Green",
+                        quantity: 6
+                    },
+                    {
+                        name: "Strawberry",
+                        color: "Red",
+                        quantity: 1
+                    },
+                    {
+                        name: "Banana",
+                        color: "Yellow",
+                        quantity: 3
+                    },
+                    
+                ]
+
+                var oModel = new JSONModel(list);
+                this.getView().setModel(oModel, "listModel");
 
             },
             onHello: function(){
